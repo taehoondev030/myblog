6959b54124e8130f78f45b0cfd898c7b62e75e22
taehoondev030 <taehoondev030@gmail.com>
2025-04-08 23:56:20 +0900

    Create AddArticleRequest (DTO)

---
+++ src/main/java/com/taehoondev/myblog/dto/AddArticleRequest.java
@@ -0,0 +1,23 @@
+package com.taehoondev.myblog.dto;
+
+import com.taehoondev.myblog.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // 기본 생성자 추가
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
+@Getter
+public class AddArticleRequest {
+
+    private String title;
+    private String content;
+
+    // DTO를 엔티티로 만들어주는 메서드
+    public Article toEntity() {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
