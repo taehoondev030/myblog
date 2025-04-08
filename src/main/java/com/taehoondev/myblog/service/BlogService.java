d913dee25e4b2dbafe62421213f49c3d1b461e97
taehoondev030 <taehoondev030@gmail.com>
2025-04-09 00:00:46 +0900

    Create BlogService file

---
+++ src/main/java/com/taehoondev/myblog/service/BlogService.java
@@ -0,0 +1,19 @@
+package com.taehoondev.myblog.service;
+
+import com.taehoondev.myblog.domain.Article;
+import com.taehoondev.myblog.dto.AddArticleRequest;
+import com.taehoondev.myblog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final 혹은 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 서블릿 컨테이너에 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // AddArticleRequest 클래스에 저장된 값들을 데이터베이스에 저장
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
