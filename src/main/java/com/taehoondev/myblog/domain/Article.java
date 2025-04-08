7d96c81ee3f456391a18ebf38fa6c4d5650a7a7d
taehoondev030 <taehoondev030@gmail.com>
2025-04-08 23:50:15 +0900

    Create Article entity

---
+++ src/main/java/com/taehoondev/myblog/domain/Article.java
@@ -0,0 +1,30 @@
+package com.taehoondev.myblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 접근 제어자가 protected인 기본 생성자 생성
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
