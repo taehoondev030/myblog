80f676738a15463d35d046938bfa44e0fda5a3cd
taehoondev030 <taehoondev030@gmail.com>
2025-04-10 02:33:00 +0900

    add GET /articles/{id}

--- src/main/java/com/taehoondev/myblog/controller/BlogApiController.java
+++ src/main/java/com/taehoondev/myblog/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import com.taehoondev.myblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +35,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 
 }
