86184feca32f07e818d320a54087a52332bba1c1
taehoondev030 <taehoondev030@gmail.com>
2025-04-10 02:00:31 +0900

    add findAll method
    add findAll method to get all articles

--- src/main/java/com/taehoondev/myblog/service/BlogService.java
+++ src/main/java/com/taehoondev/myblog/service/BlogService.java
@@ -6,6 +6,8 @@ import com.taehoondev.myblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final 혹은 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 서블릿 컨테이너에 등록
 public class BlogService {
@@ -16,4 +18,10 @@ public class BlogService {
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
     }
+
+    // 데이터베이스에서 전체 게시물 가져오기
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+    
 }
