6f1a43cb8b07800995a43b67efd424aeb3187aec
taehoondev030 <taehoondev030@gmail.com>
2025-04-10 02:03:11 +0900

    create ArticleResponse

---
+++ src/main/java/com/taehoondev/myblog/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package com.taehoondev.myblog.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+        private final String title;
+        private final String content;
+
+    public ArticleResponse(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
