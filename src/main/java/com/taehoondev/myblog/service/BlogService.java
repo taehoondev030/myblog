43c269dc6b49be99f3bc4d92146f8cb86c20adf0
taehoondev030 <taehoondev030@gmail.com>
2025-04-14 13:29:04 +0900

    add update service method

--- src/main/java/com/taehoondev/myblog/service/BlogService.java
+++ src/main/java/com/taehoondev/myblog/service/BlogService.java
@@ -2,9 +2,11 @@ package com.taehoondev.myblog.service;
 
 import com.taehoondev.myblog.domain.Article;
 import com.taehoondev.myblog.dto.AddArticleRequest;
+import com.taehoondev.myblog.dto.UpdateArticleRequest;
 import com.taehoondev.myblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -33,6 +35,15 @@ public class BlogService {
     // 게시물 삭제하기
     public void delete(long id) {
         blogRepository.deleteById(id);
+    }
+
+    // 게시물 수정하기
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+        return article;
     }
 
 }
